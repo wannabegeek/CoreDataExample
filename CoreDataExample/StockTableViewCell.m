64153c69489eb459a6293eccc46b51e8d17d0dd5
Tom Fewster <tom@wannabegeek.com>
2013-01-11 10:54:07 +0000

    Fixed issue with cells not indenting content in edit mode

--- CoreDataExample/StockTableViewCell.m
+++ CoreDataExample/StockTableViewCell.m
@@ -10,4 +10,54 @@
 
 @implementation StockTableViewCell
 
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if (nil != (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])) {
+        [self initFixedTableViewCell];
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+
+    [self initFixedTableViewCell];
+}
+
+- (void)initFixedTableViewCell {
+    for (NSInteger i = self.constraints.count - 1; i >= 0; i--) {
+        NSLayoutConstraint *constraint = [self.constraints objectAtIndex:i];
+
+        id firstItem = constraint.firstItem;
+        id secondItem = constraint.secondItem;
+
+        BOOL shouldMoveToContentView = YES;
+
+        if ([firstItem isDescendantOfView:self.contentView]) {
+            if (NO == [secondItem isDescendantOfView:self.contentView]) {
+                secondItem = self.contentView;
+            }
+        }
+        else if ([secondItem isDescendantOfView:self.contentView]) {
+            if (NO == [firstItem isDescendantOfView:self.contentView]) {
+                firstItem = self.contentView;
+            }
+        }
+        else {
+            shouldMoveToContentView = NO;
+        }
+
+        if (shouldMoveToContentView) {
+            [self removeConstraint:constraint];
+            NSLayoutConstraint *contentViewConstraint = [NSLayoutConstraint constraintWithItem:firstItem
+                                                                                     attribute:constraint.firstAttribute
+                                                                                     relatedBy:constraint.relation
+                                                                                        toItem:secondItem
+                                                                                     attribute:constraint.secondAttribute
+                                                                                    multiplier:constraint.multiplier
+                                                                                      constant:constraint.constant];
+            [self.contentView addConstraint:contentViewConstraint];
+        }
+    }
+}
+
 @end
